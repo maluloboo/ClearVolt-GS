76bcc7a43ff13e1628a3c3313af6ef00dcea35a1
maluloboo <malu.lobo3@gmail.com>
2024-11-21 18:14:40 -0300

    Criado Controller de Role e Usuario

---
+++ ClearVolt/Controllers/RoleController.cs
@@ -0,0 +1,72 @@
+﻿using ClearVolt.Domain.Models;
+using ClearVolt.DTO.Role;
+using ClearVolt.Interfaces;
+using ClearVolt.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClearVolt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly IRoleInterface _RoleInterface;
+
+        public RoleController(IRoleInterface RoleInterface)
+        {
+            _RoleInterface = RoleInterface;
+        }
+
+        [HttpGet("ListarRole")]
+        public async Task<ActionResult<RespostaModel<List<RoleModel>>>> ListarRole()
+        {
+            var Role = await _RoleInterface.ListarRole();
+            return Ok(Role);
+        }
+
+        [HttpGet("ListarRolePorId/{id_role}")]
+        public async Task<ActionResult<RespostaModel<List<RoleModel>>>> ListarRolePorId(int id_role)
+        {
+            var Role = await _RoleInterface.ListarRolePorId(id_role);
+            return Ok(Role);
+        }
+
+        [HttpPost("CriarRole")]
+        public async Task<ActionResult<RespostaModel<RoleModel>>> CriarRole(RoleCreateDto roleCreateDto)
+        {
+            var Role = await _RoleInterface.CriarRole(roleCreateDto);
+            return Ok(Role);
+        }
+
+        [HttpPut("EditarRole/{id_role}")]
+        public async Task<ActionResult<RespostaModel<RoleModel>>> EditarRole(int id_role, [FromBody] RoleEditDto roleEditDto)
+        {
+            if (id_role != roleEditDto.id_role)
+            {
+                return BadRequest("Id na URL e no corpo são diferentes");
+            }
+
+            var Role = await _RoleInterface.EditarRole(roleEditDto);
+
+            if (Role.Dados == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("DeletarRole/{id_role}")]
+        public async Task<ActionResult<RespostaModel<RoleModel>>> DeletarRole(int id_role)
+        {
+            var user = await _RoleInterface.DeletarRole(id_role);
+
+            if (user.Dados == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ ClearVolt/Controllers/UsuarioController.cs
@@ -0,0 +1,72 @@
+﻿using ClearVolt.Domain.Models;
+using ClearVolt.DTO.Usuario;
+using ClearVolt.Interfaces;
+using ClearVolt.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClearVolt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioInterface _usuarioInterface;
+
+        public UsuarioController(IUsuarioInterface usuarioInterface)
+        {
+            _usuarioInterface = usuarioInterface;
+        }
+
+        [HttpGet("ListarUsuarios")]
+        public async Task<ActionResult<RespostaModel<List<UsuarioModel>>>> ListarUsuarios()
+        {
+            var usuarios = await _usuarioInterface.ListarUsuarios();
+            return Ok(usuarios);
+        }
+
+        [HttpGet("ListarUsuarioPorId/{id_usuario}")]
+        public async Task<ActionResult<RespostaModel<List<UsuarioModel>>>> ListarUsuarioPorId(int id_usuario)
+        {
+            var usuario = await _usuarioInterface.ListarUsuarioPorId(id_usuario);
+            return Ok(usuario);
+        }
+
+        [HttpPost("CriarUsuario")]
+        public async Task<ActionResult<RespostaModel<UsuarioModel>>> CriarUsuario(UsuarioCreateDto usuarioCreateDto)
+        {
+            var usuario = await _usuarioInterface.CriarUsuario(usuarioCreateDto);
+            return Ok(usuario);
+        }
+
+        [HttpPut("EditarUsuario/{id_usuario}")]
+        public async Task<ActionResult<RespostaModel<UsuarioModel>>> EditarUsuario(int id_usuario, [FromBody] UsuarioEditDto usuarioEditDto)
+        {
+            if (id_usuario != usuarioEditDto.id_usuario)
+            {
+                return BadRequest("Id na URL e no corpo são diferentes");
+            }
+
+            var usuario = await _usuarioInterface.EditarUsuario(usuarioEditDto);
+
+            if (usuario.Dados == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("DeletarUsuario/{id_usuario}")]
+        public async Task<ActionResult<RespostaModel<UsuarioModel>>> DeletarUsuario(int id_usuario)
+        {
+            var user = await _usuarioInterface.DeletarUsuario(id_usuario);
+
+            if (user.Dados == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
