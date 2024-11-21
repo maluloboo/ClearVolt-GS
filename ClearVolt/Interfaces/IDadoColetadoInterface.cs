34abb2e1c7c6dd651e62d453b0f93dc5d702f34f
maluloboo <malu.lobo3@gmail.com>
2024-11-21 20:42:27 -0300

    Service de DadoColetado configurado

--- ClearVolt/Interfaces/IDadoColetadoInterface.cs
+++ ClearVolt/Interfaces/IDadoColetadoInterface.cs
@@ -1,6 +1,15 @@
-﻿namespace ClearVolt.Interfaces
+﻿using ClearVolt.Domain.Models;
+using ClearVolt.DTO.DadoColetado;
+using ClearVolt.Models;
+
+namespace ClearVolt.Interfaces
 {
     public interface IDadoColetadoInterface
     {
+        Task<RespostaModel<List<DadoColetadoModel>>> ListarDadoColetados();
+        Task<RespostaModel<DadoColetadoModel>> ListarDadoColetadoPorId(int id_dado);
+        Task<RespostaModel<DadoColetadoModel>> CriarDadoColetado(DadoColetadoCreateDto dadoColetadoCreateDto);
+        Task<RespostaModel<DadoColetadoModel>> EditarDadoColetado(DadoColetadoEditDto dadoColetadoEditDto);
+        Task<RespostaModel<DadoColetadoModel>> DeletarDadoColetado(int id_dado);
     }
 }
